4776a1908eef6cc25bbae2577430385365e8cc21
Davi Emer <daviemer2003@hotmail.com>
2022-10-25 21:33:37 -0300

    curso completo de design de componentes com acessibilidade

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DisabledControlModule } from './shared/components/yes-no-button-group/directives/disabled-control/disabled-control.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DisabledControlModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
